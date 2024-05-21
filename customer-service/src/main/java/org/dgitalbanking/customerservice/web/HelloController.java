ea4af52391af4ac8e9daf755ab793b51f8045085
abdelhakim telmoudy <telmoudy.abdelhakim@gmail.com>
2024-05-21 21:32:22 +0200

    first commit

---
+++ customer-service/src/main/java/org/dgitalbanking/customerservice/web/HelloController.java
@@ -0,0 +1,13 @@
+package org.dgitalbanking.customerservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello !!!!!!!!!!! customer-service";
+    }
+}
